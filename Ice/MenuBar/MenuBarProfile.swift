971bce09d705053ed8f31cebf7e17578ec1eccb5
Jordan Baird <jordanbaird.dev@gmail.com>
2024-03-23 06:07:12 -0600

    Initial setup of `MenuBarProfile`

--- Ice/MenuBar/MenuBarProfile.swift
+++ Ice/MenuBar/MenuBarProfile.swift
@@ -3,6 +3,77 @@
 //  Ice
 //
 
+import Foundation
+
+// MARK: - MenuBarProfile
+
 struct MenuBarProfile {
-    
+    var name: String
+    var visibleItems = [MenuBarItemInfo]()
+    var hiddenItems = [MenuBarItemInfo]()
+    var alwaysHiddenItems = [MenuBarItemInfo]()
+}
+
+// MARK: Default Profile
+extension MenuBarProfile {
+    /// The default menu bar profile.
+    static let defaultProfile = MenuBarProfile(
+        name: "Default",
+        visibleItems: [.iceIcon],
+        hiddenItems: [.newItems],
+        alwaysHiddenItems: []
+    )
+}
+
+// MARK: MenuBarProfile: Codable
+extension MenuBarProfile: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case name = "Name"
+        case visibleItems = "Visible"
+        case hiddenItems = "Hidden"
+        case alwaysHiddenItems = "AlwaysHidden"
+    }
+}
+
+// MARK: - MenuBarItemInfo
+
+extension MenuBarProfile {
+    struct MenuBarItemInfo {
+        let bundleIdentifier: String
+        let title: String
+    }
+}
+
+// MARK: MenuBarItemInfo Constants
+extension MenuBarProfile.MenuBarItemInfo {
+    /// Information for an item that represents the Ice icon.
+    static let iceIcon = Self(bundleIdentifier: Constants.bundleIdentifier, title: "IceIcon")
+
+    /// Information for a special item that indicates the location
+    /// where new menu bar items should appear.
+    static let newItems = Self(bundleIdentifier: "Ice", title: "NewItems")
+}
+
+// MARK: MenuBarItemInfo: Codable
+extension MenuBarProfile.MenuBarItemInfo: Codable {
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let string = try container.decode(String.self)
+        let components = string.components(separatedBy: "/")
+        guard components.count == 2 else {
+            throw DecodingError.dataCorrupted(
+                DecodingError.Context(
+                    codingPath: container.codingPath,
+                    debugDescription: "Expected 2 components, found \(components.count)"
+                )
+            )
+        }
+        self.bundleIdentifier = components[0]
+        self.title = components[1]
+    }
+
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(bundleIdentifier + "/" + title)
+    }
 }
