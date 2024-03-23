830ef60de8bb0b5de64c21cafc73a99c287fc23b
Jordan Baird <jordanbaird.dev@gmail.com>
2024-03-23 06:56:01 -0600

    Updates

--- Ice/MenuBar/MenuBarProfile.swift
+++ Ice/MenuBar/MenuBarProfile.swift
@@ -15,6 +15,53 @@ final class MenuBarProfile {
         self.name = name
         self.itemConfiguration = itemConfiguration
     }
+
+    /// Adds the given items to the section in the profile's configuration
+    /// that contains the ``MenuBarItemInfo/newItems`` special item.
+    func addItems(_ items: [MenuBarItemInfo]) {
+        for keyPath: WritableKeyPath<MenuBarItemConfiguration, _> in [\.visibleItems, \.hiddenItems, \.alwaysHiddenItems] {
+            if let index = itemConfiguration[keyPath: keyPath].firstIndex(of: .newItems) {
+                itemConfiguration[keyPath: keyPath].insert(contentsOf: items, at: index)
+                break
+            }
+        }
+    }
+}
+
+extension MenuBarProfile {
+    /// Gets the current menu bar item information, for use in a profile.
+    ///
+    /// - Parameters:
+    ///   - itemManager: The menu bar item manager to use to get the items.
+    ///   - display: The display to get the items for.
+    static func getCurrentMenuBarItemInfo(itemManager: MenuBarItemManager, display: DisplayInfo) -> [MenuBarItemInfo] {
+        let items = itemManager.getMenuBarItems(for: display, onScreenOnly: false)
+        let info: [MenuBarItemInfo] = items.compactMap { item in
+            guard
+                // immovable items and items owned by Ice should not be included in profiles
+                item.acceptsMouseEvents,
+                item.owningApplication != .current,
+                // items without a bundle identifier or title should not be included in profiles
+                let namespace = item.owningApplication?.bundleIdentifier,
+                let title = item.title
+            else {
+                return nil
+            }
+
+            // assuming the user does not have other menu bar manager apps running, off screen
+            // items are either being hidden by Ice or macOS; items hidden by Ice should be
+            // included in profiles, and items hidden by macOS should be excluded; they are
+            // hidden by macOS if their x origin is greater or equal to the minX of the display
+            if !item.isOnScreen && item.frame.origin.x >= display.frame.minX {
+                return nil
+            }
+
+            return MenuBarItemInfo(namespace: namespace, title: title)
+        }
+
+        // profiles represent items in reversed order from how they appear on screen
+        return info.reversed()
+    }
 }
 
 // MARK: Default Profile
@@ -26,22 +73,8 @@ extension MenuBarProfile {
     /// item manager and display to read the current menu bar items.
     static func createDefaultProfile(with itemManager: MenuBarItemManager, display: DisplayInfo) -> MenuBarProfile {
         let profile = MenuBarProfile(name: defaultProfileName, itemConfiguration: .defaultConfiguration)
-        profile.itemConfiguration.hiddenItems += itemManager
-            .getMenuBarItems(for: display, onScreenOnly: false)
-            .filter { item in
-                item.acceptsMouseEvents &&
-                item.owningApplication != .current
-            }
-            .compactMap { item in
-                guard
-                    let namespace = item.owningApplication?.bundleIdentifier,
-                    let title = item.title
-                else {
-                    return nil
-                }
-                return MenuBarItemInfo(namespace: namespace, title: title)
-            }
-            .reversed()
+        let info = getCurrentMenuBarItemInfo(itemManager: itemManager, display: display)
+        profile.addItems(info)
         return profile
     }
 }
