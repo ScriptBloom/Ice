109b09b6ea9ede2c77a611c4f0741d307a1db21b
Jordan Baird <jordanbaird.dev@gmail.com>
2024-03-23 06:06:26 -0600

    Updates

--- Ice/Extensions/Logger/Logger+initWithCategory.swift
+++ Ice/Extensions/Logger/Logger+initWithCategory.swift
@@ -6,8 +6,6 @@
 import Foundation
 import OSLog
 
-private let subsystem = Bundle.main.bundleIdentifier! // swiftlint:disable:this force_unwrapping
-
 extension Logger {
     /// Creates a logger using the default subsystem and the
     /// specified category.
@@ -15,6 +13,6 @@ extension Logger {
     /// - Parameter category: The string that the system uses
     ///   to categorize emitted signposts.
     init(category: String) {
-        self.init(subsystem: subsystem, category: category)
+        self.init(subsystem: Constants.bundleIdentifier, category: category)
     }
 }
