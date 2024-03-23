50d248fa3eaaaf6fe328aacae3003899af20de88
Jordan Baird <jordanbaird.dev@gmail.com>
2024-03-23 06:46:50 -0600

    Complete `WindowInfo` struct

--- Ice/Utilities/WindowInfo.swift
+++ Ice/Utilities/WindowInfo.swift
@@ -8,30 +8,44 @@ import Cocoa
 /// Information for a window.
 struct WindowInfo {
     let windowID: CGWindowID
-    let frame: CGRect
-    let title: String?
+    let backingType: CGWindowBackingType
     let windowLayer: Int
+    let frame: CGRect
+    let sharingState: CGWindowSharingType
+    let alpha: CGFloat
     let owningApplication: NSRunningApplication?
+    let memoryUsage: Int
+    let title: String?
     let isOnScreen: Bool
+    let usesVideoMemory: Bool
 
     init?(info: CFDictionary) {
         guard
             let info = info as? [CFString: CFTypeRef],
             let windowID = info[kCGWindowNumber] as? CGWindowID,
+            let backingType = (info[kCGWindowStoreType] as? UInt32).flatMap({ CGWindowBackingType(rawValue: $0) }),
+            let windowLayer = info[kCGWindowLayer] as? Int,
             let frameRaw = info[kCGWindowBounds],
             CFGetTypeID(frameRaw) == CFDictionaryGetTypeID(),
             let frame = CGRect(dictionaryRepresentation: frameRaw as! CFDictionary), // swiftlint:disable:this force_cast
-            let windowLayer = info[kCGWindowLayer] as? Int,
-            let ownerPID = info[kCGWindowOwnerPID] as? Int
+            let sharingState = (info[kCGWindowSharingState] as? UInt32).flatMap({ CGWindowSharingType(rawValue: $0) }),
+            let alpha = info[kCGWindowAlpha] as? CGFloat,
+            let ownerPID = info[kCGWindowOwnerPID] as? Int,
+            let memoryUsage = info[kCGWindowMemoryUsage] as? Int
         else {
             return nil
         }
         self.windowID = windowID
-        self.frame = frame
-        self.title = info[kCGWindowName] as? String
+        self.backingType = backingType
         self.windowLayer = windowLayer
+        self.frame = frame
+        self.sharingState = sharingState
+        self.alpha = alpha
         self.owningApplication = NSRunningApplication(processIdentifier: pid_t(ownerPID))
+        self.memoryUsage = memoryUsage
+        self.title = info[kCGWindowName] as? String
         self.isOnScreen = info[kCGWindowIsOnscreen] as? Bool ?? false
+        self.usesVideoMemory = info[kCGWindowBackingLocationVideoMemory] as? Bool ?? false
     }
 
     /// Gets an array of the current windows.
